9904f3ca552a76d8b2b82990a1095a677c0c9020
Joyce Vitória Nascimento Rodrigues <joyce.vitoria.nascimento.rodrigues@ccc.ufcg.edu.br>
2026-01-21 12:05:42 -0300

    feat - implement palavrasPlural function to count plural words in a list

---
+++ haskell/exerciciosHaskell2/palavrasPlural.hs
@@ -0,0 +1,13 @@
+--Defina uma função que recebe uma lista de palavras, e retorna a 
+--quantidade de palavras que estão no plural. 
+--Considere que qualquer palavra terminada em 's' está no plural.
+   
+palavrasPlural :: [String] -> Int
+palavrasPlural [] = 0
+palavrasPlural (x:xs)
+    | last x == 's' = plurais + 1
+    | otherwise = plurais
+    where plurais = palavrasPlural xs
+
+main = do 
+    print (palavrasPlural ["gatos", "cachorro", "pássaros", "elefante", "leões"])
