c11787f13b3de6be46c7b0ba22ab8007949f5e5a
Joyce Vitória Nascimento Rodrigues <joyce.vitoria.nascimento.rodrigues@ccc.ufcg.edu.br>
2026-01-21 12:43:07 -0300

    feat - implement printEvenNumbersBetween function to display even numbers between two integers

--- haskell/01_fundamentos/exercicios/printEvenNumbersBetween.hs
+++ haskell/01_fundamentos/exercicios/printEvenNumbersBetween.hs
@@ -2,7 +2,13 @@
 -- pares entre dois valores → even numbers between two values.
 -- Define a function that receives two integers and prints all even numbers between them.
 
+printEvenNumbersBetween :: Int -> Int -> [Int]
+printEvenNumbersBetween x y = [z | z <- [x..y], even z]
 
+
+main :: IO()
+main = do
+    print (printEvenNumbersBetween 1 10)
 
 -- Português	Inglês	Contexto
 -- número par	even	matemática
