0d8009fefbd1e11be9d089e973f84f2454eb2faa
Joyce Vitória Nascimento Rodrigues <joyce.vitoria.nascimento.rodrigues@ccc.ufcg.edu.br>
2026-01-28 16:52:34 -0300

    feat - implement ehprimo function to check if a positive integer is prime

--- haskell/01_fundamentos/exercicios/primo.hs
+++ haskell/01_fundamentos/exercicios/primo.hs
@@ -1,5 +1,16 @@
 -- Defina a função ehprimo que verifica se um determinado inteiro positivo é primo.
 --- prime number é um número inteiro maior que 1 que só é divisível por 1 e por ele mesmo.
 
-ehprimo :: Int -> Bool
-ehprimo n =
+proxDivisor :: Int -> Int -> Int 
+proxDivisor p i 
+    | mod p i == 0 = i  -- Se i divide p, retorna i
+    | otherwise = proxDivisor p (i+1)
+
+primo :: Int -> Bool
+primo 1 = False
+primo p 
+    | proxDivisor p 2 == p = True 
+    | otherwise = False 
+
+main :: IO()
+main = print (primo 7)
