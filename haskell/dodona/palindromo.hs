0d6150fc392293c829397203657fcd2de81833e0
Joyce Vitória <joyce.vitoria.nascimento.rodrigues@ccc.ufcg.edu.br>
2026-02-25 21:18:38 -0300

    add question about palindromo

--- haskell/dodona/palindromo.hs
+++ haskell/dodona/palindromo.hs
@@ -1,6 +1,16 @@
-isPalindrome :: Eq a => [a] -> Bool
-isPalindrome lista = lista == reverse lista
+-- isPalindrome :: Eq a => [a] -> Bool
+-- isPalindrome lista = lista == reverse lista
 
+-- main = do
+--    print(isPalindrome [1,2,3,2,1])
+--    print(isPalindrome "madamimadam")
+
+
+isPalindrome :: Eq a => [a] -> Bool
+isPalindrome [x] = True
+isPalindrome [] = True
+isPalindrome (x:xs) = x == last xs && isPalindrome (take (length xs - 1) xs)
+--isPalindrome (x:xs) = (x == last xs) && isPalindrome (init xs) --init traz tudo menos o último
 main = do
-   print(isPalindrome [1,2,3,2,1])
+   print(isPalindrome [1,2,2,1])
    print(isPalindrome "madamimadam")
